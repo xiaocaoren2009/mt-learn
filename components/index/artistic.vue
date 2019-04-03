752f30d9d5ceec38e0695896635bae41ead24f49
xiaocaoren2009 <xiaocaoren2009@qq.com>
2019-04-03 22:18:32 +0800

    page组件index-page开发中

---
+++ components/index/artistic.vue
@@ -0,0 +1,103 @@
+<template>
+  <section class="m-istyle">
+    <dl @mouseover="mouseover">
+      <dt>有格调</dt>
+      <dd :class="{active:kind==='all'}" kind="all" keyword="景点">
+        全部
+      </dd>
+      <dd>
+        约会聚餐
+      </dd>
+      <dd>
+        丽人SPA
+      </dd>
+      <dd>
+        电影演出
+      </dd>
+      <dd>
+        品质出游
+      </dd>
+    </dl>
+    <ul class="ibody">
+      <li v-for="item in list.all" :key="item.title">
+        <el-card :body-style="{ padding: '0px' }" shadow="never" >
+          <img :src="item.img" class="image">
+          <ul class="cbody">
+            <li class="title">{{ item.title }}</li>
+            <li class="sub-title">{{ item.subtitle }}</li>
+            <li class="price">¥<em>{{ item.price }}</em></li>
+            <li class="pos">{{ item.pos }}</li>
+          </ul>
+        </el-card>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        kind: "all",
+        list: {
+          all: [{
+            title: '大董',
+            subtitle: '糖醋黄鱼五四套餐',
+            img: 'https://p0.meituan.net/msmerchant/f0f72ccd78773cc5a0156b1c00d588c0961404.jpg@736w_416h_1e_1c',
+            price: '1296',
+            pos: '王府井/东单'
+            },{
+            title: '元祖食品',
+            subtitle: '蛋糕3选1，约10英寸',
+            img: 'https://p0.meituan.net/msmerchant/99c455bf54275263f48f9592197a4864740268.jpg@736w_416h_1e_1c',
+            price: '275',
+            pos: '双井'
+            },{
+            title: '大董',
+            subtitle: '糖醋黄鱼五四套餐',
+            img: 'https://p0.meituan.net/msmerchant/f0f72ccd78773cc5a0156b1c00d588c0961404.jpg@736w_416h_1e_1c',
+            price: '1296',
+            pos: '王府井/东单'
+            },{
+            title: '元祖食品',
+            subtitle: '蛋糕3选1，约10英寸',
+            img: 'https://p0.meituan.net/msmerchant/99c455bf54275263f48f9592197a4864740268.jpg@736w_416h_1e_1c',
+            price: '275',
+            pos: '双井'
+            },{
+            title: '大董',
+            subtitle: '糖醋黄鱼五四套餐',
+            img: 'https://p0.meituan.net/msmerchant/f0f72ccd78773cc5a0156b1c00d588c0961404.jpg@736w_416h_1e_1c',
+            price: '1296',
+            pos: '王府井/东单'
+            },{
+            title: '元祖食品',
+            subtitle: '蛋糕3选1，约10英寸',
+            img: 'https://p0.meituan.net/msmerchant/99c455bf54275263f48f9592197a4864740268.jpg@736w_416h_1e_1c',
+            price: '275',
+            pos: '双井'
+            }
+          ],
+          part: [],
+          spa: [],
+          movie: [],
+          travel: []
+       }
+      };
+    },
+    computed: {
+      cur: () => {
+        return this.list[this.kind];
+      }
+    },
+    methods: {
+      mouseover() {
+
+      }
+    }
+  }
+</script>
+
+<style scope lang="scss">
+  @import "@/assets/css/index/artistic.scss";
+</style>
---
+++ components/index/menu.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="m-menu">
+    <dl @mouseleave="mouseleave" class="nav">
+      <dt>全部分类</dt>
+      <dd 
+        v-for="(item,idx) in menu" 
+        :key="idx"
+        @mouseenter="mouseenter(item.type)">
+        <i :class="item.type" />{{item.name}}<span class="arrow"/>
+      </dd>
+    </dl>
+    <div v-if="kind" class="detail" @mouseenter="sover" @mouseleave="sout">
+      <template v-for="(item,idx) in curdetail">
+        <h4 :key="idx">{{item.title}}</h4>
+        <span v-for="v in item.child">
+          {{ v }}
+        </span>
+      </template>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+  		return {
+  		  kind:"",
+  			menu: [
+  				{
+            type: 'food',
+            name: '美食',
+            child: [{
+              title: "美食",
+              child: ["代金券", "甜点饮品",'火锅', '自助餐', '小吃快餐']
+            }]
+          },
+          {
+            type: 'takeout',
+            name: '外卖',
+            child: [{
+              title: "外卖",
+              child: ["美团外卖"]
+            }]
+          },
+          {
+            type: 'hotel',
+            name: '酒店',
+            child: [{
+              title: "酒店星际",
+              child: ["经济型", "舒适/三星", "高档/四星", "豪华/五星"]
+            }]
+          }
+        ]
+      }
+    },
+
+    computed:{
+      curdetail() {
+        return this.menu.find(v => v.type === this.kind).child;
+      }
+    },
+
+    methods:{
+     mouseenter(type) {
+       this.kind = type;       
+     },
+     mouseleave() {
+       this._timer = setTimeout(() => {
+         this.kind = "";
+       }, 150);
+     },
+     sover() {
+       clearTimeout(this._timer);
+     },
+     sout() {
+       this.kind = ""
+     }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
